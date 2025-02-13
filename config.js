0ad0b1088dac49428236d122750519fdb35a82ce
hajinoori511 <noorizaykahn889@gmail.com>
2025-02-13 07:47:59 +0430

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~OGZwHKTK#xxM7ipFY8WEAZNAYADfSDctOG6Nv1cIWVIQsCJ_A55g",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -19,15 +19,15 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "SHABAN-MD-V5",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD-V5",
+STICKER_NAME: process.env.STICKER_NAME || "𓆩•.𓇚اوزبــོـیــ ؔـڪ𓇚.•𓆪     .       .            .        𓆰𓍼͢𝐍͢𝐱͢𝐱͢𝐫͢𝐢𓂃.ོ•𓆪",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "93784717965",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Mʀ✰Sʜᴀʙᴀɴ",
 // add bot owner name
@@ -39,7 +39,7 @@ LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *✦sʜᴀʙᴀɴ-sᴏʙx-ᴍ
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
